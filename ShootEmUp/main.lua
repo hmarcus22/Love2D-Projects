45ba4d8a45ced0a4d937d0f1fa06e4273e9316a6
marols01 <marcus.olsson@sr.se>
2025-06-26 13:45:53 +0200

    Added bullets

--- ShootEmUp/main.lua
+++ ShootEmUp/main.lua
@@ -1,5 +1,13 @@
+bullets = {}
+
 function love.load()
     player = {x = 400, y = 300, speed = 200}
+end
+
+function love.keypressed(key)
+    if key == "space" then
+        table.insert(bullets, {x = player.x + 16, y = player.y})
+    end
 end
 
 function love.update(dt)
@@ -15,9 +23,20 @@ function love.update(dt)
     if love.keyboard.isDown("right") then
         player.x = player.x + player.speed * dt
     end
-    -- Add left, right, down similarly
+
+    for i = #bullets, 1, -1 do
+        local b = bullets[i]
+        b.y = b.y -300 * dt
+        if b.y < 0 then
+            table.remove(bullets, i)
+        end
+    end
+
 end
 
 function love.draw()
     love.graphics.rectangle("fill", player.x, player.y, 32, 32)
+    for _, b in ipairs(bullets) do
+        love.graphics.rectangle("fill", b.x, b.y, 4, 10)
+    end
 end
